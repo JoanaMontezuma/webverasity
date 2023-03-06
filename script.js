0cadd87bd42344fae413904770468077e92168a1
Joana Montezuma <119960830+JoanaMontezuma@users.noreply.github.com>
2023-03-06 15:07:26 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function before(){
+    document.getElementById('myImage')
+    .src="tournament.png";
+    document.getElementById('message')
+    .innerHTML="Hii! GeeksforGeeks people";
+}
+
+function middle(){
+    document.getElementById('myImage')
+    .src="tournament2.png";
+    document.getElementById('message')
+    .innerHTML="Bye! GeeksforGeeks people";
+}
+      
+function afterr(){
+    document.getElementById('myImage')
+    .src="tournament3.png";
+    document.getElementById('message')
+    .innerHTML="Bye! GeeksforGeeks people";
+}
